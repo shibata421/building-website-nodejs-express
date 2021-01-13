6a43f51f73ee9a526819084032eae9c405193df5
Fernando Shibata <shibata.421@gmail.com>
2021-01-13 16:54:03 -0300

    Modify server.js to respond get requests with static html files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-// requires express
+// modules required
 const express = require('express');
+const path = require('path');
 
 // creates an instance of express, which is the application
 const app = express();
@@ -7,8 +8,15 @@ const app = express();
 // express applications usually listen on port 3000
 const port = 3000;
 
+// This is a middleware that allows us to use the static folder content
+app.use(express.static(path.join(__dirname, './static')));
+
 app.get('/', (request, response) => {
-    response.send('Hello Express :)');
+    response.sendFile(path.join(__dirname, './static/index.html'))
+});
+
+app.get('/speakers', (request, response) => {
+    response.sendFile(path.join(__dirname, './static/speakers.html'))
 });
 
 app.listen(port, () => {
