2bdd6eb27d0edf79e1d5f3731a761726604d1312
Fernando Shibata <shibata.421@gmail.com>
2021-01-14 09:49:20 -0300

    Render the main page using ejs

--- server.js
+++ server.js
@@ -10,13 +10,18 @@ const port = 3000;
 
 // ejs doenst need to be required. express finds it after you config
 app.set('view engine', 'ejs'); // view engine is the config we want to change
-app.set('views', path.join(__dirname, './views'))
+app.set('views', path.join(__dirname, './views'));
 
 // This is a middleware that allows us to use the static folder content
 app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/index.html'));
+  /* now, it'll use ejs to render the index page. 
+    This code says to ejs to find index.ejs inside views/pages
+    The object contains local variables that will be available 
+    to the template
+    */
+  response.render('pages/index', { pageTitle: 'Welcome' });
 });
 
 app.get('/speakers', (request, response) => {
